fa40d5b410e170c0b55d586816a2c4a332eeba51
Tambua Evaristus <evaristustambua@gmail.com>
2022-07-25 12:16:10 +0100

    vercel test

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>Hello Next Js</h1>
+      <h1>Hello Next Js updated home</h1>
     </div>
   );
 }
