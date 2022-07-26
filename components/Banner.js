5a7a730dc9ca61cc24402f35e8eb60ae274c051e
Tambua Evaristus <evaristustambua@gmail.com>
2022-07-26 22:43:35 +0100

    lattest cahnge

--- components/Banner.js
+++ components/Banner.js
@@ -7,15 +7,11 @@ function Banner() {
         <div className="container px-4 px-lg-5 h-100">
           <div className="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
             <div className="col-lg-8 align-self-end">
-              <h1 className="text-white font-weight-bolder">
-SCRATCH RECORDS
-              </h1>
+              <h1 className="text-white font-weight-bolder">SCRATCH RECORDS</h1>
               <hr className="divider" />
             </div>
             <div className="col-lg-8 align-self-baseline">
-              <p className="text-white-75 mb-5">
-                 THE SUBMIT OF ENTERTAINMENT
-              </p>
+              <p className="text-white-75 mb-5">THE SUBMIT OF ENTERTAINMENT</p>
               <a className="btn btn-primary btn-xl" href="#about">
                 Find Out More
               </a>
