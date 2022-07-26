be196d856b4b1a015cba6fe15dc98f9146c0e6d6
Tambua Evaristus <evaristustambua@gmail.com>
2022-07-26 13:44:39 +0100

    service / header/ and about

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import Head from "next/head";
 import Image from "next/image";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Services from "../components/Services";
+import TextSection from "../components/TextSection";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      body
+      <Banner />
+      <TextSection />
+      <Services />
     </div>
   );
 }
