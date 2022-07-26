6379b61b498fe89702e9cb6fbca5b39539a561ec
Tambua Evaristus <evaristustambua@gmail.com>
2022-07-26 23:12:31 +0100

    poper n jquery

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,9 @@ import '../styles/globals.css'
 // add bootstrap css 
 import 'bootstrap/dist/css/bootstrap.css'
 import { useEffect } from 'react';
-import 'bootstrap/dist/js/popper.min.js';
-global.jQuery = require('jquery');
-require('bootstrap');
-
+import 'jquery';
+import '@popperjs/core'; // Edit here
+// import 'bootstrap/dist/js/bootstrap.bundle';
 function MyApp({ Component, pageProps }) {
   useEffect(()=>{
     import("bootstrap/dist/js/bootstrap");
