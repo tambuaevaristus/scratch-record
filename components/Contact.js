9bd0cd3d5ea60b6afcccedd4756641e082b47daa
Tambua Evaristus <evaristustambua@gmail.com>
2022-08-10 12:44:17 +0100

    contact component  done

--- components/Contact.js
+++ components/Contact.js
@@ -1,36 +1,80 @@
 import React from "react";
+import { FaPhoneAlt,FaInstagram} from "react-icons/fa";
+import { GiMegaphone } from "react-icons/gi";
+import { FiYoutube ,FiTwitter} from "react-icons/fi";
+
+import { AiOutlineFacebook, AiOutlineMail } from "react-icons/ai";
 
 function Contact() {
   return (
     <div className="container py-5">
       <div className=" row">
-        <div className="card col-md-6 col-lg-4">
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
+        <div className=" col-md-6 col-lg-4 my-4">
+          <div className="card">
+            <div className="card-body text-center">
+              <GiMegaphone size={80} color="grey" />
+              <p className="card-text text-muted">
+                +237 674421540 <br /> +237 695770772
+              </p>
+            </div>
           </div>
         </div>
 
-        <div className="card text-center col-md-6 col-lg-4">
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
+        <div className=" col-md-6 col-lg-4 my-4">
+          <div className="card">
+            <div className="card-body text-center">
+              <AiOutlineMail size={80} color="grey" />
+              <p className="card-text text-muted">
+
+              rholfterence@gmail.com
+
+              </p>
+              <p></p>
+
+            </div>
+          </div>
+        </div>
+        <div className=" col-md-6 col-lg-4 my-4">
+          <div className="card">
+            <div className="card-body text-center">
+              <FiYoutube size={80} color="grey" />
+              <p className="card-text text-muted">
+                Scratch <br /> Records
+              </p>
+            </div>
           </div>
         </div>
 
-        <div className="card col-md-6 col-lg-4">
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content.
-            </p>
+        <div className=" col-md-6 col-lg-4 my-4">
+          <div className="card">
+            <div className="card-body text-center">
+              <FaInstagram size={80} color="grey" />
+              <p className="card-text text-muted">
+                Scratch <br /> Records
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className=" col-md-6 col-lg-4 my-4">
+          <div className="card">
+            <div className="card-body text-center">
+              <AiOutlineFacebook size={80} color="grey" />
+              <p className="card-text text-muted">
+                Scratch <br /> Records
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className=" col-md-6 col-lg-4 my-4">
+          <div className="card">
+            <div className="card-body text-center">
+              <FiTwitter size={80} color="grey" />
+              <p className="card-text text-muted">
+                Scratch <br /> Records
+              </p>
+            </div>
           </div>
         </div>
       </div>
--- components/SubBanner.js
+++ components/SubBanner.js
@@ -9,7 +9,7 @@ function SubBanner(props) {
     <img src='/assets/banners/banner3.jpg'className="w-100" height="250"  />
     <div className='mask ' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
       <div className='d-flex justify-content-center my-5 align-items-center h-100'>
-        <h1 className='text-white fw-bolder display-2'>{props.page}</h1>
+        <h1 className='text-white fw-bolder fade-in display-2'>{props.page}</h1>
       </div>
     </div>
   </div>
